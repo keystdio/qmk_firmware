fff9d8ff4fed560cc447dc30afe754ce80afd2ce
Kuangyou Yao <kyao@amazon.com>
2018-11-21 21:54:42 -0800

    Fixed some typos

--- keyboards/ergodox_ez/keymaps/key_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/key_osx/keymap.c
@@ -13,18 +13,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |   =  |           |   -  |   6  |   7  |   8  |   9  |   0  |Backspac|
+ * |    `   |   1  |   2  |   3  |   4  |   5  |   =  |           |   -  |   6  |   7  |   8  |   9  |   0  |Backspac|
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Cmd+L|           | Cmd+R|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
  * |--------+------+------+------+------+------| Shi+L|           | Shi+R|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  |Alt/Ct| LCmd | Left | Right|                                       |  Up  | Down |   [  |   ]  |  L2  |
+ *   |  L1  |Alt/Ct| LCmd | Left | Right|                                       |  Up  | Down |   [  |]/Ctrl|  L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  |Ctr+Sp|       |Shi+F6|Ctrl/Esc|
+ *                                        | Esc  |Ctr+Sp|       |Shi+F6|Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space| Enter|------|       |------|  Enter |Enter |
@@ -35,21 +35,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,       KC_1,          KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
+        KC_GRV,       KC_1,          KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
         KC_TAB,       KC_Q,          KC_W,   KC_E,   KC_R,   KC_T,   LCMD(KC_LEFT),
         KC_CAPS,      KC_A,          KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,      KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   S(KC_LEFT),
         MO(SYMB),     CTL_T(KC_LALT),KC_LGUI,KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  LCTL(KC_SPC),
+                                              KC_ESC,  LCTL(KC_SPC),
                                                               KC_HOME,
                                               KC_SPC,KC_ENTER,KC_END,
                                                
         // right hand
         KC_MINS,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_BSPC,
-        LCMD(KC_LEFT),KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+        LCMD(KC_RIGHT),KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                       KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
-        S(KC_RIGHT),  KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                              RGUI_T(KC_UP),  KC_DOWN,KC_LBRC,KC_RBRC,          MO(MDIA),
+        S(KC_RIGHT),  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+                              RGUI_T(KC_UP),  KC_DOWN,KC_LBRC, CTL_T(KC_RBRC),MO(MDIA),
         LSFT(KC_F6),  CTL_T(KC_ESC),
         KC_PGUP,
         KC_PGDN,KC_ENTER, KC_ENTER
@@ -165,7 +165,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+  ergodox_led_all_set(LED_BRIGHTNESS_HI);
 };
 
 // Runs constantly in the background, in a loop.
